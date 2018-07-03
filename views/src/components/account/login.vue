8b6d17e54775915c8d45da772ca420bfb5b045db
kaso <kaso@kasos-MacBook-Pro.local>
2018-07-04 00:59:55 +0800

    Revert "完成基础路由配置"
    This reverts commit ff6bc15d2c583518327c59fbbe5f886f9b8ae89e.

--- views/src/components/account/login.vue
+++ views/src/components/account/login.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="row">
-    <div class="col-xs-12 col-md-5">
+    <div class="col">
       <form>
         <div class="form-group">
-          <label>英文名</label>
-          <input type="email" class="form-control">
+          <label for="exampleInputEmail1">英文名</label>
+          <input type="email" class="form-control" placeholder="英文名谁也不知道放心骂">
         </div>
         <div class="form-group">
-          <label>密码</label>
-          <input type="password" class="form-control">
+          <label for="exampleInputPassword1">密码</label>
+          <input type="password" class="form-control" placeholder="请输入密码">
         </div>
         <div class="form-group">
-          <font class="form-text text-muted">* 您的账户将自动注册，请您保管好密码</font>
+        <small id="emailHelp" class="form-text text-muted">* 您的账户将自动被注册 <br/> * 请保管好您的账号</small>
         </div>
         <button type="submit" class="btn btn-primary">登陆</button>
       </form>
@@ -20,12 +20,11 @@
 </template>
 
 <script>
-  export default {};
+export default {};
 </script>
 
 <style scoped>
-  .col-xs-12,
-  .col-md-5 {
-    margin-top: 16px;
-  }
+.col{
+margin-top: 16px
+}
 </style>
--- views/src/main.js
+++ views/src/main.js
@@ -7,21 +7,6 @@ import 'bootstrap/dist/js/bootstrap'
 import 'jquery'
 import 'popper.js'
 
-// 配置vue-router
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
-import routers from './router'
-
-const router = new VueRouter({
-  mode: 'history',
-  routes:routers
-})
-
-console.log(routers)
-
-
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
